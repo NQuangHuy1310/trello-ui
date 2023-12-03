3c8488eb159cf551180264bb9b6903c1540de8b7
Nguyen Quang Huy <kaisa13102004@gmail.com>
2023-12-04 05:11:17 +0700

    relative_absolute_import

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import theme from './theme'
+import App from '~/App.jsx'
+import theme from '~/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
